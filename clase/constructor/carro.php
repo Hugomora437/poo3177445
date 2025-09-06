511509a5f1b6724b67d4f498e60f1903e7ddc18c
hugofabianmora <hugofabianmora2020@gmail.com>
2025-09-05 20:17:41 -0500

    creacion del constructor

---
+++ clase/constructor/carro.php
@@ -0,0 +1,33 @@
+<?php
+
+class carro{
+    public $color;
+    public $marca;
+    public $modelo;
+    public $velocidad;
+    public $caballaje;
+    public $puestos;
+
+
+    // public function __construct()
+    // {
+    //     $this->color = "Rojo";
+    //     $this->marca = "Ferrari";
+    //     $this->modelo = "Gallardo";
+    //     $this->velocidad = 500;
+    //     $this->caballaje = 300;
+    //     $this->puestos = 2;
+    // }  
+
+    public function __construct($color, $marca, $modelo, $velocidad, $caballaje, $puestos)
+    {
+        $this->color = $color;
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->velocidad = $velocidad;
+        $this->caballaje = $caballaje;
+        $this->puestos = $puestos;
+}
+
+}
+   
